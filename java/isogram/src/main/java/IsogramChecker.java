6e16b5d5df96c811c00f4a8b475fc989f3fc2acb
tsun-code <61623464+tsun-code@users.noreply.github.com>
2021-01-16 20:34:24 +0800

    Add comment to Isogram side exercise on java track

--- java/isogram/src/main/java/IsogramChecker.java
+++ java/isogram/src/main/java/IsogramChecker.java
@@ -4,6 +4,7 @@ import java.util.regex.Pattern;
 class IsogramChecker {
 
     boolean isIsogram(String phrase) {
+        // Regular Expression Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
         return ! phrase.matches(".*(?i)([a-z]).*\\1.*|.*(?i)[^a-z- ].*");
     }
 }
