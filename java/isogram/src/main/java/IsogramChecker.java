47a2fa39a76dbd404d39b83c907c0cd4b9914e56
tsun-code <61623464+tsun-code@users.noreply.github.com>
2021-01-16 20:54:16 +0800

    Add comments to explain my solution on Isogram side exercise on java track

--- java/isogram/src/main/java/IsogramChecker.java
+++ java/isogram/src/main/java/IsogramChecker.java
@@ -8,6 +8,11 @@ class IsogramChecker {
         // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
         // https://support.kobotoolbox.org/restrict_responses.html
         // https://www.regular-expressions.info/refmodifiers.html
+
+        // There are two parts in my regular expressions:
+        // First part ".*(?i)([a-z]).*\\1.*" is to test if the string has repeated characters.
+        // Second part ".*(?i)[^a-z- ].*" is to test if the string has any character other than letters, hypern and space.
+        // Either the evaluation of the first or second part is true, it is not an Isogram.
         return ! phrase.matches(".*(?i)([a-z]).*\\1.*|.*(?i)[^a-z- ].*");
     }
 }
